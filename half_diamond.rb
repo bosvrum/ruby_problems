fa77c7edb47ed75dcc4900721e8ea785c78a9853
Catalin <catalinboruga@yahoo.com>
2015-04-19 23:29:25 +0200

    Print half diamond

---
+++ half_diamond.rb
@@ -0,0 +1,13 @@
+# Print out a half diamond
+count = 0
+8.times do 
+	count += 1
+	stars = "*" * count
+	puts stars
+end
+count = 10
+8.times do 
+	count -= 1
+	stars = "*" * count
+	puts stars
+end
